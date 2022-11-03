201804b572a5bf6a875e05e0643415b118900905
Joel-100 <joelkugonza@gmail.com>
2022-11-03 20:52:33 +0300

    Develope a website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 import Calculator from './components/calculator';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Calculator />
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/calculator" element={<Calculator />} />
+          <Route exact path="/quotes" element={<Quotes />} />
+        </Routes>
       </div>
     );
   }
